79f103e10040348ccea318e72ee6bb525d087199
Sezen <sezen.ibrahim3@gmail.com>
2023-01-16 19:56:39 +0200

    Added Login with logic to add JWT token

--- Instagram/Controllers/IdentityController.cs
+++ Instagram/Controllers/IdentityController.cs
@@ -4,14 +4,22 @@
     using Instagram.Models.Identity;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Options;
+    using Microsoft.IdentityModel.Tokens;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
 
     public class IdentityController : ApiController
     {
         private readonly UserManager<User> userManager;
+        private readonly Settings appSettings;
 
-        public IdentityController(UserManager<User> userManager) =>
-                this.userManager = userManager;
-
+        public IdentityController(UserManager<User> userManager, IOptions<Settings> appSettings)
+        {
+            this.userManager = userManager;
+            this.appSettings = appSettings.Value;
+        }
 
         public async Task<IActionResult> Register(RegisterInputModel model)
         {
@@ -29,6 +37,36 @@
             }
 
             return BadRequest(result.Errors);
+        }
+
+        public async Task<ActionResult<string>> Login(LoginInputModel model)
+        {
+            var user = await this.userManager.FindByNameAsync(model.UserName);
+            if(user == null)
+            {
+                return this.Unauthorized();
+            }
+
+            var passwordValidate = await this.userManager.CheckPasswordAsync(user, model.Password);
+
+            if (!passwordValidate)
+            {
+                return this.Unauthorized();
+            }
+
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(this.appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var encryptedToken = tokenHandler.WriteToken(token);
+
+            return encryptedToken;
         }
     }
 }
---
+++ Instagram/Models/Identity/LoginInputModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Instagram.Models.Identity
+{
+    using System.ComponentModel.DataAnnotations;
+
+
+    public class LoginInputModel
+    {
+        [Required]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
