dd15e1c279f87c0a3ad3bd7a9fcf21ab10492d0e
Sezen <sezen.ibrahim3@gmail.com>
2023-01-16 19:14:58 +0200

    Improved Structure

--- Instagram/Controllers/HomeController.cs
+++ Instagram/Controllers/HomeController.cs
@@ -1,32 +1,15 @@
-﻿using Instagram.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
-namespace Instagram.Controllers
+﻿namespace Instagram.Controllers
 {
-    public class HomeController : Controller
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+
+    [ApiController]
+    [Route("{controller}")]
+    public class HomeController : ControllerBase
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public IActionResult Get()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return this.Ok("it works");
         }
     }
 }
--- Instagram/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Instagram.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
